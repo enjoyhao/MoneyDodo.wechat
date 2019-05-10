767e2b4cf487129a205212f5b73a0cb36b885a93
linshk <ggatlsk@gmail.com>
2019-05-10 15:25:33 +0800

    add westore for global state management

---
+++ store.js
@@ -0,0 +1,29 @@
+export default {
+  data: {
+    server: 'http://localhost:8888',
+    apibase: '/api',
+    motto: 'Hello World',
+    openId: null,
+    token: null,
+    userInfo: null,
+    profile: null,
+    hasUserInfo: false,
+    hasProfile: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    logs: [],
+    firstName: 'dnt',
+    lastName: 'zhang',
+    fullName: function () {
+      return this.firstName + this.lastName
+    },
+    pureProp: 'pureProp',
+    globalPropTest: 'abc', //更改我会刷新所有页面,不需要在组件和页面声明data依赖
+    ccc: { ddd: 1 } //更改我会刷新所有页面,不需要在组件和页面声明data依赖
+  },
+  globalData: ['globalPropTest', 'ccc.ddd'],
+  logMotto: function () {
+    console.log(this.data.motto)
+  },
+  //默认 false，为 true 会无脑更新所有实例
+  //updateAll: true
+}

aedfb49f02bf75a328ac00232aafbe659ed2e003
linshk <ggatlsk@gmail.com>
2019-05-18 12:35:25 +0800

    添加表单校验函数

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,36 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const rules = {
+  notEmpty: value => {
+    let status = true
+    if (!value || value == '') {
+      status = false
+    }
+    let errMsg = status ? '' : '输入不能为空'
+    return { 'status': status, 'errMsg': errMsg }
+  },
+  isNumber: value => {
+    let regex = /^[0-9]+$/
+    let status = regex.test(value)
+    let errMsg = status ? '' : '请输入纯数字'
+    return { 'status': status, 'errMsg': errMsg }
+  },
+  noInvalidChar: value => {
+    let regex = /[~!@#$%^&*()/\|,.<>?"'();:_+-=\[\]{}]/
+    let status = !regex.test(value)
+    let errMsg = status ? '' : '请勿输入特殊符号'
+    return { 'status': status, 'errMsg': errMsg }
+  },
+  isValidEmail: value => {
+    let regex = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+    let status = regex.test(value)
+    let errMsg = status ? '' : '请输入有效的邮箱'
+    return { 'status': status, 'errMsg': errMsg }
+  }
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  rules: rules
 }

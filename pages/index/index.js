bd7b3763ad515dffadb42859f6e00b84113a7314
linshk <ggatlsk@gmail.com>
2019-06-22 13:08:39 +0800

    完善首页加载任务列表功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,9 @@
 //index.js
 import store from '../../store/store.js'
 import create from '../../utils/create'
+import config from '../../api/config'
 import test from '../../api/test'
+import cpt from '../../api/cpt'
 import regeneratorRuntime from '../../libs/runtime'
 
 //获取应用实例
@@ -15,7 +17,9 @@ create(store, {
     hasProfile: null,
     canIUse: null,
     array: ['1','2','3'],
-    businessType: ['问卷调查','外卖代取','社团招新','快递代取']
+    businessType: ['问卷调查','外卖代取','社团招新','快递代取'],
+    qtnrTasks: [],
+    allQtnrTasks: [],
   },
   //事件处理函数
   bindViewTap: function() {
@@ -24,28 +28,35 @@ create(store, {
     })
   },
   onLoad: function () {
-  
-    /*else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this
+    cpt.getAllTasks().then(res => {
+      console.log(res)
+      if (res.data.status) {
+        res.data.data = that.formatTaskData(res.data.data)
+        that.setData({
+          qtnrTasks: res.data.data.filter(item => item.kind == config.TASK_KIND_QUESTIONNAIRE),
+          allQtnrTasks: res.data.data.filter(item => item.kind == config.TASK_KIND_QUESTIONNAIRE)
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }*/
+    }, err => {
+      console.log(err)
+    })
+  },
+  /**
+   * 格式化任务列表中的数据，如日期等字段
+   */
+  formatTaskData: function(tasks) {
+    return tasks.map(task => {
+      // 格式化日期
+      task.cutoff = task.cutoff || ""
+      task.cutoff = task.cutoff.replace("T", " ").replace("Z", " ")
+      task.pubdate = task.pubdate || ""
+      task.pubdate = task.pubdate.replace("T", " ").replace("Z", " ")
+      // 格式化奖励
+      task.reward = task.reward || 0
+      task.reward = task.reward > 0 ? task.reward : task.reward
+      return task
+    })
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -61,13 +72,16 @@ create(store, {
     // test.testApiTask()
     // test.testApiCpt()
     // test.wtf()
-    // test.testApiCpt()
+    test.testApiCpt()
     // test.testGetCerts()
-    wx.navigateTo({
-      url: '../design-questionnaire/design-questionnaire',
-    })
+    //wx.navigateTo({
+    //  url: '../design-questionnaire/design-questionnaire',
+    //})
     // test.testGetUser()
     // test.testGetTasks()
     // test.wtf()
+  },
+  onTapTask: function(e) {
+    console.log(e)
   }
 })
